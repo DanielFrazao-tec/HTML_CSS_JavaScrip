54d78f44015ec362d3b78130ae3efe60ace18a9d
Daniel Frazao <d_frazaomsn@hotmail.com>
2022-06-08 15:19:22 -0300

    modificaçao Script

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 'use strict'
 
+// Usando o documnet.querySelector para obter referencia do botao
 const switcher = document.querySelector('.btn');
+
+// Adicionado um ouvinte e o manipulador do evento click
+switcher.addEventListener('Click',function(){
+    document.body.classList.toggle('dark-theme')
+
+// Atualizando o rotulo do botao 
+var className = document.body.className;
+if(className == "light-theme"){
+    this.textContent = "Dart";
+}
+else{
+    this.textContent = "LIght"
+}
+
+console.log('current class name:' + className);
+});
+
